820e9eb63adf9b9f0399c5f91c453fc5f5e4902f
shreeshsingh <shreeshnarayansingh@gmail.com>
2019-09-23 20:09:58 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TodoHeaderComponent } from './todo-header/todo-header.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoHeaderComponent,
+    TodoFooterComponent,
+    TodoListComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
